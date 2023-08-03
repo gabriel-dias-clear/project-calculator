53a9db04a4f1f5474f6a0fe761eeab98f9dcf8c0
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-03 10:35:19 -0300

    events mouse insert

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,12 +1,13 @@
 class CalcController {
 
      constructor(){
-        this._locale = 'pt-BR'
+        this._locale = 'pt-BR';
         this._dateEl = document.querySelector('#data');
         this._hourEl = document.querySelector('#hora');
-        this._dispayEl = document.querySelector('#display')
+        this._dispayEl = document.querySelector('#display');
         this._currDate;
         this.init();
+        this.initButton();
 
      }
 
@@ -21,6 +22,39 @@ class CalcController {
         }, 1000)
 
      }
+
+     addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+        
+        })
+     }
+
+    
+
+     initButton(){
+
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g')
+        
+        buttons.forEach((btn, index)=>{
+            
+            this.addEventListenerAll(btn, 'click drag', e=>{
+
+                console.log(btn.className.baseVal.replace("btn-", ""))
+            
+            })
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
+
+                btn.style.cursor = 'pointer'
+            
+            })
+
+        })
+    }
+
 
      get displayTime(){
         
